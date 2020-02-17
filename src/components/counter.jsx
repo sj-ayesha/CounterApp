c2d27f7861a61182f6541fa079df123f954416a7
Ayesha Jownally <sj.ayesha@gmail.com>
2020-02-17 14:18:16 +0400

    feat: Logic

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,26 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
+  }
+
+  render() {
+    return <React.Fragment>
+      { this.state.tags.length === 0 && 'Please create a new tag!' }
+      { this.renderTags() }
+    </React.Fragment>;
   }
 
   getBadgeClasses() {
