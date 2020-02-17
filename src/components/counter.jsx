361205450b1e87979dec41225aa9c9126a903892
Ayesha Jownally <sj.ayesha@gmail.com>
2020-02-17 15:01:02 +0400

    feat: DOM Manipulation

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,21 @@ class Counter extends Component {
     tags: []
   };
 
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = product => { //Event Binding
+    console.log("Increment Clicked", this);
+    console.log('product', product);
+    this.setState({ count: this.state.count + 1 }); //Changing state on click
+  }
+  
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({ id: 1 });
+  // }
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
@@ -19,10 +34,19 @@ class Counter extends Component {
   }
 
   render() {
-    return <React.Fragment>
-      { this.state.tags.length === 0 && 'Please create a new tag!' }
-      { this.renderTags() }
-    </React.Fragment>;
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({id: 1})}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* { this.state.tags.length === 0 && 'Please create a new tag!' }
+      { this.renderTags() } */}
+      </React.Fragment>
+    );
   }
 
   getBadgeClasses() {
