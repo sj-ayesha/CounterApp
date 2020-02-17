a376143ca870d0ca36a9f2720c465469ecf864c0
Ayesha Jownally <sj.ayesha@gmail.com>
2020-02-17 16:56:48 +0400

    fear: component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: []
   };
 
@@ -14,7 +14,7 @@ class Counter extends Component {
   handleIncrement = product => { //Event Binding
     console.log("Increment Clicked", this);
     console.log('product', product);
-    this.setState({ count: this.state.count + 1 }); //Changing state on click
+    this.setState({ value: this.state.value + 1 }); //Changing state on click
   }
   
   // doHandleIncrement = () => {
@@ -34,6 +34,7 @@ class Counter extends Component {
   }
 
   render() {
+    // console.log('props', this.props)
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -51,13 +52,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
